a2bd805a2f334c049fd73c84128a1fe78189e048
Solomon Wang <solomonwang@Solomon-Wangs-MacBook.local>
2012-04-09 19:36:05 -0700

    fixed cache

--- src/edu/berkeley/cs162/KVCacheTest.java
+++ src/edu/berkeley/cs162/KVCacheTest.java
@@ -28,60 +28,91 @@ place N items in a N sized cache in order, then place an N+1th item, and then as
 	@Test
 	public void test_initialize_Small() {
 		System.out.println("Create KVCache of size 2");
-		KVCache cache = new KVCache(2);
+		KVCache<String, String> cache = new KVCache<String, String>(2);
 		assertTrue(cache != null);
 	}
 
 	@Test
 	public void test_put1_Small() {
 		System.out.println("Test if put works");
-		KVCache cache = new KVCache(2);
-		cache.put(key, value);
+		KVCache<String, String> cache = new KVCache<String, String>(2);
 		assertTrue(cache != null);
+		assertFalse( cache.put(key, value) );
 	}
 	
 	@Test
 	public void test_put2_Small() {
-		System.out.println("Test putting the same <k, v> in twice doesn't make a duplicate");
-		KVCache cache = new KVCache(2);
+		System.out.println("Test put returns true if there exists a previous <k, v> pair");
+		KVCache<String, String> cache = new KVCache<String, String>(2);
+		assertTrue(cache != null);
+		assertFalse( cache.put(key, value) );
+		assertFalse( cache.put(key1, value1) );
+		assertTrue( cache.put(key1, value1) );
+	}
+	
+	@Test 
+	public void test_del1() {
+		System.out.println("Test del with empty cache");
+		KVCache<String, String> cache = new KVCache<String, String>(2);
+		assertTrue(cache != null);
+		cache.del(key);
+	}
+	
+	@Test 
+	public void test_del2() {
+		System.out.println("Test del with empty cache");
+		KVCache<String, String> cache = new KVCache<String, String>(2);
 		assertTrue(cache != null);
 		cache.put(key, value);
-		cache.put(key1, value1);
-		cache.put(key1, value1);
+		cache.del(key);
+	}
+
+	@Test
+	public void test_get1_Small() {
+		System.out.println("Test one put and one get");
+		KVCache<String, String> cache = new KVCache<String, String>(2);
+		assertTrue(cache != null);
+		assertFalse( cache.put(key, value) );
 		assertTrue(cache.get(key) == value);
+	}
+
+	@Test
+	public void test_get2_Small() {
+		System.out.println("Test cacheSize puts and gets");
+		KVCache<String, String> cache = new KVCache<String, String>(2);
+		assertTrue(cache != null);
+		cache.put(key, value);
+		assertTrue(cache.get(key) == value);
+		cache.put(key1, value1);
+		assertTrue(cache.get(key1) == value1);
 	}
 	
 	@Test
-	public void test_put3_Small() {
+	public void test_putAndGet1_Small() {
 		System.out.println("Test put(k, v) then put(k, v1) has get(k) == v1");
-		KVCache cache = new KVCache(2);
+		KVCache<String, String> cache = new KVCache<String, String>(2);
 		assertTrue(cache != null);
 		cache.put(key, value);
+		assertTrue(cache.get(key) == value);
 		cache.put(key, value1);
 		assertTrue(cache.get(key) == value1);
 	}
 	
 	@Test
-	public void test_put4_Small() {
+	public void test_putAndGet2_Small() {
 		System.out.println("Test if the right value is evicted when cache is overloaded");
-		KVCache cache = new KVCache(2);
+		KVCache<String, String> cache = new KVCache<String, String>(2);
 		assertTrue(cache != null);
 		cache.put(key, value);
+		assertTrue(cache.get(key) == value);
 		cache.put(key1, value1);
-		cache.put(key2 ,value2);
+		assertTrue(cache.get(key1) == value1);
+		cache.put(key2, value2);
+		System.out.println("wat" + cache.get(key));
+		cache.del(key);
 		assertTrue(cache.get(key) == null);
 		assertTrue(cache.get(key1) == value1);
 		assertTrue(cache.get(key2) == value2);
-	}
-	
-	@Test
-	public void test_get() {
-	
-	}
-	
-	@Test 
-	public void test_del() {
-		
 	}
 	
 	/*
